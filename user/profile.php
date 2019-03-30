163881b7ced6117a425f087613f1018668fced19
Jeremie MARTEL <jmartel@e2r2p8.42.fr>
2019-03-30 16:57:55 +0100

    change on profile

--- user/profile.php
+++ user/profile.php
@@ -26,6 +26,7 @@
 			echo '<p>First Name : <input type="text" value="'.$_SESSION[USER][USER_FNAME].'" disabled/></p>'.PHP_EOL;
 			echo '<p>Last Name : <input type="text" value="'.$_SESSION[USER][USER_LNAME].'" disabled/></p>'.PHP_EOL;
 			echo '<p>Email address : <input type="mail" value="'.$_SESSION[USER][USER_MAIL].'" disabled/></p>'.PHP_EOL;
-		?>
+			?>
+			<a href="delete.php">Delete my account</a>
 	</body>
 </html>
